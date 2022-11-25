5de47c1924a7626d7f057b6a114dbecd02306e13
Сергей Платонов <platonov1727@yandex.ru>
2022-11-25 17:36:35 +0300

    Vse sdelano Boss

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import get_object_or_404
-
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from posts.models import Group, Post
-
 from .permissions import IsAuthorOrReadOnly
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
